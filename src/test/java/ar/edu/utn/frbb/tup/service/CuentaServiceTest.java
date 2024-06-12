7fcf03d4e6e9a91cf77796d356b71b3739d776a9
DiegoXeneize <18diegobruno18@gmail.com>
2024-06-12 02:33:28 -0300

    Test

--- src/test/java/ar/edu/utn/frbb/tup/service/CuentaServiceTest.java
+++ src/test/java/ar/edu/utn/frbb/tup/service/CuentaServiceTest.java
@@ -89,7 +89,7 @@ public class CuentaServiceTest {
         cuentaService.darDeAltaCuenta(cuentaExistente, 123456L);
 
         when(cuentaDao.find(cuentaExistente.getNumeroCuenta())).thenReturn(cuentaExistente);
-        //Inficamos que cuando llamemos al metodo getCuentas el mock devuelve una lista con el objeto cuentaExistent
+        //Inficamos que cuando llamemos al metodo getCuentas el mock devuelve una lista con el objeto cuentaExistentee
         when(cuentaDao.getCuentasByCliente(cliente.getDni())).thenReturn(Arrays.asList(cuentaExistente));
 
         Cuenta cuenta2 = new Cuenta();
